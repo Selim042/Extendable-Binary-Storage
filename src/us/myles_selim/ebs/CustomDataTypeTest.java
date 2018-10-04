9edf716a900fda09b008a2d796337950fabfe3ac
Myles Selim <Selim042@users.noreply.github.com>
2018-10-04 17:25:07 -0500

    Add test of custom data types

---
+++ src/us/myles_selim/ebs/CustomDataTypeTest.java
@@ -0,0 +1,61 @@
+package us.myles_selim.ebs;
+
+class CustomDataTypeTest {
+
+	private static class CustomData {
+
+		public int test = 2;
+
+		public CustomData(int val) {
+			this.test = val;
+		}
+
+	}
+
+	private static class DataTypeCustomData extends DataType<CustomData> {
+
+		private CustomData data;
+
+		@Override
+		public CustomData getValue() {
+			return this.data;
+		}
+
+		@Override
+		public void setValue(CustomData value) {
+			this.data = value;
+		}
+
+		@Override
+		protected void setValueObject(Object value) {
+			return;
+		}
+
+		@Override
+		public Class<?>[] accepts() {
+			return new Class[] { CustomData.class };
+		}
+
+		@Override
+		public void toBytes(Storage ebs) {
+			ebs.writeInt(data.test);
+		}
+
+		@Override
+		public void fromBytes(Storage ebs) {
+			if (data == null)
+				data = new CustomData(ebs.readInt());
+			else
+				data.test = ebs.readInt();
+		}
+
+	}
+
+	public static void main(String... args) {
+		EBStorage storage = new EBStorage().registerType(9, new DataTypeCustomData());
+		storage.set("customData", new CustomData(42));
+		CustomData dat = storage.get("customData", CustomData.class);
+		System.out.println(dat.test);
+	}
+
+}
