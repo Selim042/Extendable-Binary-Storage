beb200935770bce89f02e9064dedb1e1d7ec63d3
Myles Selim <Selim042@users.noreply.github.com>
2018-10-06 13:48:55 -0500

    Test IOHelper

--- src/test/java/us/myles_selim/ebs/IOTest.java
+++ src/test/java/us/myles_selim/ebs/IOTest.java
@@ -1,8 +1,6 @@
 package us.myles_selim.ebs;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.File;
 
 public class IOTest {
 
@@ -11,23 +9,9 @@ public class IOTest {
 		storage.set("testInt", 5);
 		storage.set("string", "bleh");
 		storage.set("testDouble", 123.455);
+		IOHelper.writeEBStorage(storage, new File("ioHelperTest.ebs"));
 
-		byte[] serializedWrite = storage.serialize();
-		try {
-			FileOutputStream out = new FileOutputStream("ioTest.ebs");
-			out.write(serializedWrite);
-			out.close();
-		} catch (IOException e) {}
-
-		byte[] serializedRead = null;
-		try {
-			FileInputStream in = new FileInputStream("ioTest.ebs");
-			serializedRead = new byte[in.available()];
-			in.read(serializedRead);
-			in.close();
-		} catch (IOException e) {}
-
-		EBStorage storage2 = EBStorage.deserialize(serializedRead);
+		EBStorage storage2 = IOHelper.readEBStorage(new File("ioHelperTest.ebs"));
 		System.out.println(storage2.get("testInt"));
 		System.out.println(storage2.get("string"));
 		System.out.println(storage2.get("testDouble"));
