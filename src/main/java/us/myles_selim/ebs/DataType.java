adab365112f1615b6c76dcd66b3a8047b0eeeece
Myles Selim <Selim042@users.noreply.github.com>
2018-10-06 13:42:17 -0500

    Move the abstract data type out of the package (I need the access the protected methods)

---
+++ src/main/java/us/myles_selim/ebs/DataType.java
@@ -0,0 +1,24 @@
+package us.myles_selim.ebs;
+
+public abstract class DataType<V> {
+
+	public abstract V getValue();
+
+	public abstract void setValue(V value);
+
+	protected abstract void setValueObject(Object value);
+
+	public abstract Class<?>[] accepts();
+
+	public boolean acceptsValue(Object value) {
+		for (Class<?> c : accepts())
+			if (c != null && c.isInstance(value))
+				return true;
+		return false;
+	}
+
+	public abstract void toBytes(Storage stor);
+
+	public abstract void fromBytes(Storage stor);
+
+}
